fb9c4c444fbc073485051074ce9db29daf167acb
Jenny Skoglund <jenny.skoglund@iths.se>
2024-09-25 16:56:28 +0200

    uppdatering lab 2

--- Labs/Lab02_machine_learning.py
+++ Labs/Lab02_machine_learning.py
@@ -1,31 +1,69 @@
+# Datapoints
 path = "Data/datapoints.txt" # Gå in i fil, open folder, välj den mapp som du vill öppna vs code med. (Python-programming-JENNY_SKOGLUND)
 
-with open (path, "r") as f:
+with open(path, "r") as f:
     text = f.read()
 
-print(repr(text))
+# print(repr(text))
 
 import re
 
-quotes, i = [], 1
+quotes = []
 
 with open(path, "r") as f_read, open("Data/datapoints_new.txt", "w") as f_write:
     f_write.write("Pokemon list\n\n")
     for quote in f_read:
-        quote = quote.strip(" \n")
-        quote = re.sub(r" +", " ", quote)
+        #quote = quote.strip(" \n")
+        #quote = re.sub(r" +", " ", quote)
         if quote != "":
-            f_write.write(f"{i}. {quote}\n")
-            i += 1
+            f_write.write(f"{quote}\n")
+            #i += 1
+# nedan sorterar listan i datapoints, för att få ut 0 = pichu och 1 = pikachu.
+pichu = []
+pikachu = []
 
+with open("Data/datapoints_new.txt", "r") as list:
+    for line in list:
+        line_separate = line.strip()
+        if line_separate:
+            text_split = line_separate.split(",")
+
+            try:
+                width = round(float(text_split[0].strip()), 2)
+                hight = round(float(text_split[1].strip()), 2)
+                label = int(text_split[2].strip())
+
+                if label == 0:
+                    pichu.append([width, hight, label])
+                elif label == 1:
+                    pikachu.append([width, hight, label])
+            except ValueError:
+                print(f"Wrong in lines in the loop 'try' for float and int.")
+                
 import matplotlib.pyplot as plt
-import numpy as np
+pichu_x = [x[0] for x in pichu]
+pichu_y = [y[1] for y in pichu]
+pikachu_x = [x[0] for x in pikachu]
+pikachu_y = [y[1] for y in pikachu]
 
-width1 = 
-height1 =
-width0 = 
-height0 = 
+plt.title("Pikachu and Pichus width and hight. Pikachu as Pink, Pichu as blue")
+plt.xlabel("Pikachu and Pichus X variable")
+plt.ylabel("Pikachu and Pichus Y variable")
+plt.scatter(pichu_x, pichu_y, marker="*", color="pink")
+plt.scatter(pikachu_x, pikachu_y, marker="*", color="purple")
 
-plt.scatter(width1, height1, color = 'hotpink')
-plt.scatter(width0, height0, color = 'blue')
+plt.show()
 
+# Test points
+
+path_test = "Data/testpoints.txt" 
+
+with open(path_test, "r") as f:
+    test_points = f.read()
+
+with open("Data/testpoints.txt", "r") as test_list:
+    for line in test_list:
+        line_separate_test = line.strip()
+        if line_separate_test:
+            text_split_test = line_separate_test.split(",")
+        print(text_split_test)
